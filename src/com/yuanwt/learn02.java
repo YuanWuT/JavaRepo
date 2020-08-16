b6ca9aaf07743f246104099d917d5e2ea1be04ad
yuanwt <18317509809@163.com>
2020-08-16 14:31:31 +0800

    重新增加learn02

---
+++ src/com/yuanwt/learn02.java
@@ -0,0 +1,13 @@
+package com.yuanwt;
+
+/**
+ * @Auther:yuanwt
+ * @Date:2020/8/16 - 08 - 16 - 14:29
+ * @Description: com.yuanwt
+ * @Version: 1.0
+ */
+public class learn02 {
+    public static void main(String[] args) {
+        System.out.println("learn02");
+    }
+}
