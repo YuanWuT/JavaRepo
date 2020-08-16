1bfc505a5f560409766fd57ea7b79e5bcd2bdb66
yuanwt <18317509809@163.com>
2020-08-16 13:50:34 +0800

    Leran01上传Github

---
+++ src/com/yuanwt/learn01.java
@@ -0,0 +1,14 @@
+package com.yuanwt;
+
+/**
+ * @Auther:yuanwt
+ * @Date:2020/8/16 - 08 - 16 - 13:40
+ * @Description: com.yuanwt
+ * @Version: 1.0
+ */
+public class learn01 {
+    public static void main(String[] args) {
+        System.out.println("lrarn01");
+
+    }
+}
